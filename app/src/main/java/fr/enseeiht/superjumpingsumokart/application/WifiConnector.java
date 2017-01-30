14ecef4f660d7b9a5b4300e99c8a0fabcd99f9c8
Romain Verset <romain.verset@gmail.com>
2017-01-30 15:44:59 +0100

    WifiConnector

---
+++ app/src/main/java/fr/enseeiht/superjumpingsumokart/application/WifiConnector.java
@@ -0,0 +1,188 @@
+package fr.enseeiht.superjumpingsumokart.application;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.parrot.arsdk.ardiscovery.ARDISCOVERY_PRODUCT_ENUM;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDevice;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceNetService;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryException;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryService;
+import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiver;
+import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiverDelegate;
+
+import java.util.List;
+
+/**
+ * @author Romain Verset
+ * This class allow the application to remotly connect with a Parrot drone.
+ * Created by Romain Verset on 30/01/17.
+ */
+
+class WifiConnector implements ARDiscoveryServicesDevicesListUpdatedReceiverDelegate {
+
+    /**
+     * The logging tag. Useful for debugging purpose.
+     */
+    private final static String WIFI_CONNECTOR_TAG = "WIFI_CONNECTOR";
+
+    /**
+     * The context of the {@link android.app.Activity} running this connector.
+     */
+    private final Context APP_CONTEXT;
+
+    /**
+     * The discovery service.
+     */
+    private ARDiscoveryService discoveryService;
+
+    /**
+     * The connection service.
+     */
+    private ServiceConnection connectionService;
+
+    /**
+     * The receiver for notification about available devices.
+     */
+    private ARDiscoveryServicesDevicesListUpdatedReceiver discoveryServiceDeviceListUpdateReceiver;
+
+    /**
+     * The list of all available devices.
+     */
+    private List<ARDiscoveryDeviceService> devicesList;
+
+    /**
+     * The remote device connected with this connector.
+     */
+    private ARDiscoveryDevice device;
+
+    /**
+     * Default constructor of the class. (Romain Verset - 30/01/2017)
+     * Sets up the context and starts the discovering service.
+     * @param appContext The context of the android {@link android.app.Activity} running this connector.
+     */
+    private WifiConnector(Context appContext) {
+        APP_CONTEXT = appContext;
+        start();
+    }
+
+    /**
+     * Starts the discovery service. (Romain Verset - 30/01/2017)
+     * This method first check if a connection is available. If so, it then tries to discover drones
+     * to connect with.
+     */
+    private void start() {
+        Log.d(WIFI_CONNECTOR_TAG, "Starting services...");
+        // First checks if a connection is already established
+        if (connectionService == null) {
+            connectionService = new ServiceConnection() {
+
+                // When a connection is gotten, initializes the device discovery.
+                @Override
+                public void onServiceConnected(ComponentName name, IBinder service) {
+                    discoveryService = ((ARDiscoveryService.LocalBinder) service).getService();
+                    discoveryService.start();
+                }
+
+                // When the connection is closed or lost, stops the discovery service if it exists
+                // and deletes the discovery service.
+                @Override
+                public void onServiceDisconnected(ComponentName name) {
+                    if (discoveryService != null) {
+                        discoveryService.stop();
+                    }
+                }
+            };
+            registerReceivers();
+        }
+
+        // If the discovery service does not exist yet, creates it and binds it to the
+        // application context and starts it. Otherwise just starts it.
+        if (discoveryService == null) {
+            Intent discoveryIntent = new Intent(APP_CONTEXT, ARDiscoveryService.class);
+            APP_CONTEXT.bindService(discoveryIntent, connectionService, Context.BIND_AUTO_CREATE);
+        } else {
+            discoveryService.start();
+        }
+    }
+
+    /**
+     * Stops every services started by this connector. (Romain Verset - 30/01/2017)
+     * It runs a separate thread to properly unbind and close the connection service and  to close
+     * the discovery service.
+     */
+    public void stop()
+    {
+        Log.d(WIFI_CONNECTOR_TAG, "Closing services...");
+        if (discoveryService != null) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    unregisterReceivers();
+                    discoveryService.stop();
+                    APP_CONTEXT.unbindService(connectionService);
+                    connectionService = null;
+                    discoveryService = null;
+                }
+            }).start();
+        }
+    }
+
+    /**
+     * Register this connector so that it receives notification about changes in the list of available
+     * devices.
+     */
+    private void registerReceivers() {
+        discoveryServiceDeviceListUpdateReceiver = new ARDiscoveryServicesDevicesListUpdatedReceiver(this);
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(APP_CONTEXT);
+        localBroadcastManager.registerReceiver(discoveryServiceDeviceListUpdateReceiver, new IntentFilter(ARDiscoveryService.kARDiscoveryServiceNotificationServicesDevicesListUpdated));
+    }
+
+    /**
+     * Unregister this connector so that it no longer receives notification about changes in the
+     * list of available devices.
+     */
+    private void unregisterReceivers() {
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(APP_CONTEXT);
+        localBroadcastManager.unregisterReceiver(discoveryServiceDeviceListUpdateReceiver);
+    }
+
+    /**
+     * Creates a device using a discovery service. (Romain Verset - 30/01/2017)
+     * @param discoveryDeviceService The discovery service used to construct the device.
+     * @return The device corresponding to the given {@link ARDiscoveryDeviceService}.
+     */
+    public ARDiscoveryDevice createDevice(ARDiscoveryDeviceService discoveryDeviceService) {
+        if (discoveryDeviceService != null && ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_ARDRONE.equals(ARDiscoveryService.getProductFromProductID(discoveryDeviceService.getProductID()))) {
+            try {
+                device = new ARDiscoveryDevice();
+                ARDiscoveryDeviceNetService netService = (ARDiscoveryDeviceNetService) discoveryDeviceService.getDevice();
+                device.initWifi(ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_ARDRONE, netService.getName(), netService.getIp(), netService.getPort());
+            } catch (ARDiscoveryException arde) {
+                Log.e(WIFI_CONNECTOR_TAG, "Unable to create device : " + arde.getMessage());
+            }
+        }
+        return device;
+    }
+
+    /**
+     * (Romain Verset - 30/01/2017)
+     * @return The device currently connected with this connector.
+     */
+    public ARDiscoveryDevice getDevice() {
+        return device;
+    }
+
+    // Describes the action to do when the a new connection is available or when
+    @Override
+    public void onServicesDevicesListUpdated() {
+        Log.d(WIFI_CONNECTOR_TAG, "Devices list updated : " + devicesList.size() + " devices available.");
+    }
+}
