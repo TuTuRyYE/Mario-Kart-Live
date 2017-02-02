4bfcf87cb8a94405aac80f6899fa2ea96df700ea
Romain Verset <versetr@bde.enseeiht.fr>
2017-02-02 16:19:30 +0100

    Revert "DroneController update (spinning jum + test animation added)"
    This reverts commit 8c70f395caadc30fa4b92a70b02f83ea0373fb7d

--- app/src/main/java/fr/enseeiht/superjumpingsumokart/application/DroneController.java
+++ app/src/main/java/fr/enseeiht/superjumpingsumokart/application/DroneController.java
@@ -58,7 +58,7 @@ public class DroneController implements ARDeviceControllerListener, ARDeviceCont
      */
     public DroneController(GUIGame guiGame, ARDiscoveryDevice device) {
         GUI_GAME = guiGame;
-        DRONE = new Drone("JUMPY", new Vector3D(0,0,0),new TestItem(new Vector3D(0,0,0)), new Vector3D(0,0,0));
+        DRONE = new Drone(new Vector3D(0,0,0),new TestItem(), new Vector3D(0,0,0));
         try {
             deviceController = new ARDeviceController(device);
             deviceController.addListener(this);
