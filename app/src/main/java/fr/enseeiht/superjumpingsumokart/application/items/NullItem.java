52b453381c76426b49eb3976ecc3b29d48ae9c43
michelmatthieu <michel.matthieu.ensues@gmail.com>
2017-02-06 11:31:25 +0100

    commit NullItem

---
+++ app/src/main/java/fr/enseeiht/superjumpingsumokart/application/items/NullItem.java
@@ -0,0 +1,33 @@
+package fr.enseeiht.superjumpingsumokart.application.items;
+
+import android.widget.ImageButton;
+
+import fr.enseeiht.superjumpingsumokart.application.DroneController;
+
+/**
+ * Created by michelmatthieu on 06/02/2017.
+ */
+
+public class NullItem extends Item {
+
+    private final static String  NAME = "banana";
+
+    /**
+     * Default constructor of the class {@link Item}. (Matthieu Michel - 06/02/2017).
+     *
+     * @param name name given to the Item.
+     */
+    NullItem() {
+        super(NAME);
+    }
+
+    @Override
+    public void applyEffect(DroneController sender, DroneController receiver) {
+
+    }
+
+    @Override
+    public void assignResource(ImageButton ib) {
+
+    }
+}
