4771d2026d79a343bc4ccbc5977a4569e2f4be12
michelmatthieu <michel.matthieu.ensues@gmail.com>
2017-02-20 10:41:31 +0100

    commit pour LucasBG31

--- app/src/main/java/fr/enseeiht/superjumpingsumokart/application/GUICircuit.java
+++ app/src/main/java/fr/enseeiht/superjumpingsumokart/application/GUICircuit.java
@@ -2,7 +2,6 @@ package fr.enseeiht.superjumpingsumokart.application;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
