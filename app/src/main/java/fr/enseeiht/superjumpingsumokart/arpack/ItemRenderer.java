54be95829e28f676d000cca66b2a8be193b50da8
Romain Verset <romain.verset@gmail.com>
2017-02-23 15:11:55 +0100

    AR -> Affichage de modèles 3D pour les objets

--- app/src/main/java/fr/enseeiht/superjumpingsumokart/arpack/ItemRenderer.java
+++ app/src/main/java/fr/enseeiht/superjumpingsumokart/arpack/ItemRenderer.java
@@ -4,7 +4,6 @@ import android.util.Log;
 
 import org.artoolkit.ar.base.ARToolKit;
 import org.artoolkit.ar.base.rendering.ARRenderer;
-import org.artoolkit.ar.base.rendering.Cube;
 
 import javax.microedition.khronos.opengles.GL10;
 
@@ -17,16 +16,14 @@ class ItemRenderer extends ARRenderer {
 
     private final static String ITEM_RENDERER_TAG = "ItemRenderer";
 
-    private int markerID = -1;
-    private Cube cube = new Cube(40.0f, 0.0f, 0.0f, 20.0f);
-
     /**
      * Markers can be configured here.
      */
     public boolean configureARScene() {
         Log.d(ITEM_RENDERER_TAG, "configureARScene() called.");
-        markerID = ARToolKit.getInstance().addMarker("single;Data/hiro.patt;80");
-        if (markerID < 0) return false;
+        //markerID = ARToolKit.getInstance().addMarker("single;Data/hiro.patt;80");
+        ARToolKit.getInstance().addModel("Data/models/giantbanana.obj", "single;Data/hiro.patt;80", 0, 20f, true);
+        //ARToolKit.getInstance().addModel("Data/models/Ferrari_Modena_Spider.obj", "single;Data/patt.kanji;80", 1);
         return true;
     }
 
@@ -43,28 +40,7 @@ class ItemRenderer extends ARRenderer {
      */
     @Override
     public void draw(GL10 gl) {
-
-        Log.d(ITEM_RENDERER_TAG, "draw() function called.");
-
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-
-        // Apply the ARToolKit projection matrix
-        gl.glMatrixMode(GL10.GL_PROJECTION);
-        gl.glLoadMatrixf(ARToolKit.getInstance().getProjectionMatrix(), 0);
-
-        gl.glEnable(GL10.GL_CULL_FACE);
-        gl.glShadeModel(GL10.GL_SMOOTH);
-        gl.glEnable(GL10.GL_DEPTH_TEST);
-        gl.glFrontFace(GL10.GL_CW);
-
-        // If the marker is visible, apply its transformation, and draw a cube
-        if (ARToolKit.getInstance().queryMarkerVisible(markerID)) {
-            gl.glMatrixMode(GL10.GL_MODELVIEW);
-            gl.glLoadMatrixf(ARToolKit.getInstance().queryMarkerTransformation(markerID), 0);
-            cube.draw(gl);
-        }
+        ARToolKit.getInstance().drawModels();
     }
 
 }
