9882070f42257ede02b62a442f7c761397264d8d
michelmatthieu <michel.matthieu.ensues@gmail.com>
2017-02-27 14:44:57 +0100

    commit getObject + suppression de postion dans BluetoothCom

--- app/src/main/java/fr/enseeiht/superjumpingsumokart/application/Circuit.java
+++ app/src/main/java/fr/enseeiht/superjumpingsumokart/application/Circuit.java
@@ -43,6 +43,9 @@ public class Circuit {
      */
     private int checkPointToCheck;
 
+    public HashMap<DetectionTask.symbols, Item> getObjects() {
+        return objects;
+    }
 
     private static Circuit circuitInstance;
 
@@ -51,7 +54,7 @@ public class Circuit {
      * @param laps
      * @param checkPointToCheck
      */
-    public Circuit(int laps, int checkPointToCheck) {
+   private Circuit(int laps, int checkPointToCheck) {
         this.lapsNumber = laps;
         this.checkPointToCheck = checkPointToCheck;
         this.markers = new HashMap<>();
@@ -69,7 +72,7 @@ public class Circuit {
      * Initialises the singleton instance of {@link Circuit}.
      * @param laps The number of laps for the circuit.
      */
-    static void initInstance(int laps, int checkPointToCheck) {
+    public static void initInstance(int laps, int checkPointToCheck) {
         if (circuitInstance == null) {
             circuitInstance = new Circuit(laps, checkPointToCheck);
         }
