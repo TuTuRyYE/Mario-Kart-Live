24047e7953818a4dbba317123aee94ebaf656d67
Romain Verset <romain.verset@gmail.com>
2017-02-28 16:57:01 +0100

    Correction ItemRenderer + DetectionTask + MàJ des bibliothèques

--- app/src/main/java/fr/enseeiht/superjumpingsumokart/arpack/ItemRenderer.java
+++ app/src/main/java/fr/enseeiht/superjumpingsumokart/arpack/ItemRenderer.java
@@ -29,33 +29,19 @@ public final class ItemRenderer extends ARRenderer {
      */
     public boolean configureARScene() {
         Log.d(ITEM_RENDERER_TAG, "configureARScene() called.");
-        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.HIRO,ARToolKit.getInstance().addMarker("multi;Data/patt.hiro;80"));
+        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.HIRO,ARToolKit.getInstance().addMarker("single;Data/patt.hiro;80"));
         Log.d(ITEM_RENDERER_TAG,Integer.toString(SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.HIRO)));
-        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.KANJI, ARToolKit.getInstance().addMarker("multi;Data/patt.kanji;80"));
+        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.KANJI, ARToolKit.getInstance().addMarker("single;Data/patt.kanji;80"));
         Log.d(ITEM_RENDERER_TAG,Integer.toString(SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.KANJI)));
-        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.A, ARToolKit.getInstance().addMarker("single;Data/patt.a;80"));
-        Log.d(ITEM_RENDERER_TAG,Integer.toString(SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.A)));
-        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.B, ARToolKit.getInstance().addMarker("single;Data/patt.b;80"));
-        Log.d(ITEM_RENDERER_TAG,Integer.toString(SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.B)));
-        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.C, ARToolKit.getInstance().addMarker("single;Data/patt.c;80"));
-        Log.d(ITEM_RENDERER_TAG,Integer.toString(SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.C)));
-        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.D, ARToolKit.getInstance().addMarker("single;Data/patt.d;80"));
-        Log.d(ITEM_RENDERER_TAG,Integer.toString(SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.D)));
-        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.F, ARToolKit.getInstance().addMarker("single;Data/patt.f;80"));
-        Log.d(ITEM_RENDERER_TAG,Integer.toString(SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.F)));
-        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.G, ARToolKit.getInstance().addMarker("single;Data/patt.g;80"));
-        Log.d(ITEM_RENDERER_TAG,Integer.toString(SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.G)));
-        int magicBoxId = SYMBOLS_HASH_MAP.get(DetectionTask.Symbol.A);
-        ARToolKit.getInstance().addModel("Data/models/box.obj", "single;Data/patt.hiro;80", magicBoxId, 100.0f, true);
+        SYMBOLS_HASH_MAP.put(DetectionTask.Symbol.A, ARToolKit.getInstance().addModel("Data/models/box.obj", "single;Data/patt.hiro;80", DetectionTask.Symbol.A.ordinal(), 20.0f, true));
         return true;
     }
 
     public void defineModelAtSymbol(Item item, DetectionTask.Symbol symbol) {
         Log.d(ITEM_RENDERER_TAG, "Defined model for symbol : " + symbol.name());
         String symbolString = symbol.name().toLowerCase();
-        int id = SYMBOLS_HASH_MAP.get(symbol);
         if (item instanceof Banana) {
-            ARToolKit.getInstance().addModel("Data/models/giantbanana.obj", "single;Data/patt.".concat(symbolString).concat(";80"), id, 20.0f, true);
+            SYMBOLS_HASH_MAP.put(symbol, ARToolKit.getInstance().addModel("Data/models/giantbanana.obj", "single;Data/patt.".concat(symbolString).concat(";80"), symbol.ordinal(), 20.0f, true));
         } /*else if (item instanceof FakeBox) {
             ARToolKit.getInstance().addModel("Data/models/box.obj", "single;Data/patt.".concat(symbolString).concat(";80"), id, 20.0f, false);
         }*/
@@ -65,8 +51,7 @@ public final class ItemRenderer extends ARRenderer {
     public void deleteModelAtSymbol(DetectionTask.Symbol symbol) {
         Log.d(ITEM_RENDERER_TAG, "Deleted model for symbol : " + symbol.name());
         String symbolString = symbol.name().toLowerCase();
-        int id = SYMBOLS_HASH_MAP.get(symbol);
-        ARToolKit.getInstance().addModel("Data/models/void.obj", "single;Data/patt.".concat(symbolString).concat(";80"), id, .0f, false);
+        SYMBOLS_HASH_MAP.put(symbol, ARToolKit.getInstance().addModel("Data/models/void.obj", "single;Data/patt.".concat(symbolString).concat(";80"), symbol.ordinal(), .0f, false));
     }
 
     @Override
