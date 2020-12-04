27aa4b479e619d743c9765bb1f11f7b2b7740837
root <root@DESKTOP-39BHD6V.localdomain>
2020-12-04 09:33:18 +0100

    Add coin class and picture

---
+++ app/src/main/java/fr/enseeiht/superjumpingsumokart/application/items/Coins.java
@@ -0,0 +1,64 @@
+package fr.enseeiht.superjumpingsumokart.application.items;
+
+import android.util.Log;
+import android.widget.ImageButton;
+
+import fr.enseeiht.superjumpingsumokart.R;
+import fr.enseeiht.superjumpingsumokart.application.circuit.Circuit;
+import fr.enseeiht.superjumpingsumokart.application.DroneController;
+import fr.enseeiht.superjumpingsumokart.arpack.DetectionTask;
+
+/**
+ * Implementation of Coin{@link Item}.
+ * @author Marien AMATHIEU.
+ */
+
+public class Coin extends Item {
+
+    /**
+     * The logging tag. Useful for debugging.
+     */
+    private final static String ITEM_TAG = "Item.Coin";
+
+    /**
+     * Name of the {@link Item}.
+     */
+    private final static String NAME = "coin";
+
+    /**
+     * Default constructor of the class {@link Coin}.
+     */
+    public Coin() {
+        super(NAME);
+    }
+/*
+   @Override
+   public boolean useItem(DroneController controller, DetectionTask.Symbol symbol) {
+       if (symbol != null) {
+           Circuit.getInstance().addObject(symbol, this);
+           Log.d(ITEM_TAG, "A banana has been put on the circuit");
+           return true;
+       } else {
+           return false;
+       }
+   }
+*/
+
+    @Override
+    public boolean useItem(DroneController droneController, DetectionTask.Symbol marker) {
+        return true;
+    }
+
+    @Override
+    public void applyEffect(DroneController droneController) {
+        Log.d(ITEM_TAG, "A coin has been touched");
+        //droneController.spin();
+    }
+
+    @Override
+    public void assignResource(ImageButton ib) {
+        ib.setImageResource(R.drawable.coin);
+    }
+}
+
+
